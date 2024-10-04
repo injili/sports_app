2725479f9f79036fa1eb39270e08efdf9f6199f5
injili <nyarekigospel@gmail.com>
2024-10-04 18:42:39 +0300

    this is just a trial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-neutral-100 min-h-screen">
+      <div className="
+        w-[700px] h-[600px] bg-cyan-400 rounded-full fixed z-1 blur-[200px] translate-y-[-25%]
+      "></div>
+      <div className="
+        w-[700px] h-[600px] bg-indigo-500 founded-full fixed z-1 bottom-4 right-4
+         blur-[200px]
+      "></div>
+      <div className="flex gap-12 flex-col justify-center items-center min-h-full">
+        <div className="px-48  py-12 bg-black rounded-lg backdrop-filter backdrop-blur-[20px] bg-opacity-10 border border-indigo-400">
+          <h1 className="font-black text-6xl text-blacke">This Title</h1>
+          <hr className="my-4 border-indigo-400"/>
+          <p className="text-lg text-blacke">This is just text and should be text</p>
+        </div>
+        <div className="px-48  py-12 bg-black rounded-lg backdrop-filter backdrop-blur-[20px] bg-opacity-10 border border-indigo-400">
+          <h1 className="font-black text-6xl text-blacke">This Title</h1>
+          <hr className="my-4 border-indigo-400"/>
+          <p className="text-lg text-blacke">This is just text and should be text</p>
+        </div>        
+        <div className="px-48  py-12 bg-black rounded-lg backdrop-filter backdrop-blur-[20px] bg-opacity-10 border border-indigo-400">
+          <h1 className="font-black text-6xl text-blacke">This Title</h1>
+          <hr className="my-4 border-indigo-400"/>
+          <p className="text-lg text-blacke">This is just text and should be text</p>
+        </div>    
+        <div className="px-48  py-12 bg-black rounded-lg backdrop-filter backdrop-blur-[20px] bg-opacity-10 border border-indigo-400">
+          <h1 className="font-black text-6xl text-blacke">This Title</h1>
+          <hr className="my-4 border-indigo-400"/>
+          <p className="text-lg text-blacke">This is just text and should be text</p>
+        </div>
+    
+        <div className="px-48  py-12 bg-black rounded-lg backdrop-filter backdrop-blur-[20px] bg-opacity-10 border border-indigo-400">
+          <h1 className="font-black text-6xl text-blacke">This Title</h1>
+          <hr className="my-4 border-indigo-400"/>
+          <p className="text-lg text-blacke">This is just text and should be text</p>
+        </div>      </div>
+    </div>
   )
 }
-
-export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bowldyOneSC: ['Bowlby One SC', 'sans-serif'],
+        alata: ['Alata', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
